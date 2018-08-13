859c780e147ee8276f2f334ae6275e4e52792574
samuel <osamaimafidon@gmail.com>
2018-08-12 18:14:26 -0700

    added a modal for collecting registeration data

--- src/components/ClientWorkshop.js
+++ src/components/ClientWorkshop.js
@@ -4,49 +4,86 @@ import React, { Component } from 'react';
 import '../css/header.css';
 import Header from './Header'
 // import '../inline.js';
-import {Button, ButtonToolbar, Modal} from 'reactstrap'
+import { Button, ButtonToolbar } from 'reactstrap'
 import { Top, BodyContainer, Footer, SocialMedia } from "./util"
+
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root')
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+const inputStyles = {
+    "border": "none",
+    "borderBottom": "1px solid #f58b3b",
+    "outline":"none",
+}
+
+const buttonStyles = {
+    "border": "1px solid #f58b3b",
+    "color": "#f58b3b",
+    "background": "#fff",
+    "borderRadius": "10px",
+    "outline":"none"
+}
+
+const closeButtonStyle = {
+    "width": "50%",
+    "height": "50%",
+    "borderRadius": "25%",
+    "float":"right"
+}
 
 class ClientWorkshop extends Component {
     constructor(props) {
         super(props)
 
-let dLocation = "22 joy avenue ajao estate lagos";
- dLocation.toString().replace(/\s+/g, '+');
-// const url =`https://maps.google.com/maps?q=${dLocation}&t=k&z=19&ie=UTF8&iwloc=&output=embed`;
-    const { location: { state: { workshop } } } = this.props;
-    console.log(workshop);
-    const {startDate,endDate, time, title, fee, bannerUrl,description, id, no_of_seats, applications} = workshop;
-    console.log(workshop);
-    this.workshopId = id;
-    const top = {
-        startDate,
-        endDate,
-        time,
-        title,
-        fee,
- 
-    }
+        let dLocation = "22 joy avenue ajao estate lagos";
+        dLocation.toString().replace(/\s+/g, '+');
+        // const url =`https://maps.google.com/maps?q=${dLocation}&t=k&z=19&ie=UTF8&iwloc=&output=embed`;
+        const { location: { state: { workshop } } } = this.props;
+        console.log(workshop);
+        const { startDate, endDate, time, title, fee, bannerUrl, description, id, no_of_seats, applications } = workshop;
+        console.log(workshop);
+        this.workshopId = id;
+        const top = {
+            startDate,
+            endDate,
+            time,
+            title,
+            fee,
 
-    const bottom = {
-        bannerUrl,
-        time,
-        startDate,
-        endDate,
-        description
-    }
+        }
+
+        const bottom = {
+            bannerUrl,
+            time,
+            startDate,
+            endDate,
+            description
+        }
 
 
-    this.state ={
-        top,
-        bottom,
-        fee,
-        no_of_seats,
-        applications,
-        show:true
-    }
+        this.state = {
+            top,
+            bottom,
+            fee,
+            no_of_seats,
+            applications,
+            modalIsOpen: false,
 
-    console.log(this.state)
+        }
+
+        console.log(this.state)
 
 
         // this.state = {
@@ -65,93 +102,107 @@ let dLocation = "22 joy avenue ajao estate lagos";
 
     }
 
-      handleClose = ()=>{
-    this.setState({ show: false });
-  }
+    // getFee = ()=>(
+    //     fee
+    // )
+    openModal = () => {
+        this.setState({ modalIsOpen: true });
+    }
 
-  handleShow = ()=>{
-    this.setState({ show: true });
-  }
+    afterOpenModal = () => {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f56111';
+    }
 
-    initPayment = ()=>{
-    //     const email = prompt("Please enter email");
-    //     if((email === null) || email === "") {
-    //         alert("Email field missing");
-    //         return;
-    // }
-    //     const amount = this.state.top.fee;
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    }
+
+    initPayment = (e) => {
+        //     const email = prompt("Please enter email");
+        //     if((email === null) || email === "") {
+        //         alert("Email field missing");
+        //         return;
+        // }
+        //     const amount = this.state.top.fee;
+        e.preventDefault();
         const data = {
-            email:"osamaimafidon@gmail.com",
-            amount:25000000
+            "email": e.target.email.value,
+            "amount": this.state.top.fee,
+            "fullname":e.target.fullname.value,
+            "phone":e.target.phone.value,
+            "workshop_id":this.workshopId
         }
+        this.closeModal()
+
+        console.log(data)
         this.payWithPaystack(data)
 
     }
 
-    payWithPaystack = ({email, amount})=>{
+    payWithPaystack = ({ email, amount, fullname, phone, workshop_id }) => {
 
-    var handler = window.PaystackPop.setup({
-      key: 'pk_test_4edb6704ac9c9db84e986ca247970a5160fe5651',
-      email,
-      amount,
-      ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-      metadata: {
-         custom_fields: [
-            {
-                display_name: "Samuel",
-                variable_name: "mobile_number",
-                value: +2348012345678,
-                workshop_id:this.workshopId
+        var handler = window.PaystackPop.setup({
+            key: 'pk_test_4edb6704ac9c9db84e986ca247970a5160fe5651',
+            email,
+            amount,
+            ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+            metadata: {
+                custom_fields: [
+                    {
+                        fullname,
+                        phone,
+                        workshop_id
+                    }
+                ]
+            },
+            callback: function (response) {
+                alert('success. transaction ref is ' + response.reference);
+                console.log(response)
+            },
+            onClose: function () {
+                alert('window closed');
             }
-         ]
-      },
-      callback: function(response){
-          alert('success. transaction ref is ' + response.reference);
-          console.log(response)
-      },
-      onClose: function(){
-          alert('window closed');
-      }
-    });
-    handler.openIframe();
-  }
+        });
+        handler.openIframe();
+    }
 
     render() {
         return (
             <div>
                 <Header />
-                <Top initPayment={this.initPayment} top={this.state.top} seatsLeft={this.state.no_of_seats - this.state.applications} />
-                <BodyContainer bottom={this.state.bottom}  />
-                <SocialMedia fee={this.state.fee} seatsLeft={this.state.no_of_seats - this.state.applications} />
+                <Top openModal={this.openModal} initPayment={this.initPayment} top={this.state.top} seatsLeft={this.state.no_of_seats - this.state.applications} />
+                <BodyContainer bottom={this.state.bottom} />
+                <SocialMedia openModal={this.openModal} fee={this.state.fee} seatsLeft={this.state.no_of_seats - this.state.applications} />
                 <Footer />
-                {/*<ButtonToolbar>
-        <Button bsStyle="primary" onClick={this.handleShow}>
-          Launch demo modal
-        </Button>
+                {/*fgfhfghfhggh
+jhkjhj*/}
+                <div>
+                    {/*<button onClick={this.openModal}>Open Modal</button>*/}
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onAfterOpen={this.afterOpenModal}
+                        onRequestClose={this.closeModal}
+                        style={customStyles}
+                        contentLabel="Modal for registeration"
+                    >
+{/*
+                        <div style={closeButtonStyle}><button onClick={this.closeModal}>X</button>
+                        </div>*/}
+                        <h4 ref={subtitle => this.subtitle = subtitle}>Fill out details</h4><br />
+                        <form onSubmit={this.initPayment}>
+                            <div style={{"color":"#f58b3b"}}>Fullname</div>
+                            <p><input  name="fullname" required type="text" style={inputStyles} /></p>
+                            <div style={{"color":"#f58b3b"}}>Phone</div>
+                            <p><input name="phone" required type="tel" style={inputStyles}  /></p>
+                            <div style={{"color":"#f58b3b"}}>Email</div>
+                            <p><input  name="email" required type="email" style={inputStyles} /></p>
+                            <div style={{"float":"right"}}><button type="submit" style={buttonStyles}>Submit</button></div>
 
-        <Modal
-          {...this.props}
-          show={this.state.show}
-          onHide={this.handleHide}
-          dialogClassName="custom-modal"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-lg">
-              Modal heading
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Wrapped Text</h4>
-            <p>
-              Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae
-  cumque dolorem.
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleHide}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </ButtonToolbar>*/}
+                        </form>
+                    </Modal>
+                </div>
+                {/*gjjghjg*/}
             </div>
         )
     }
